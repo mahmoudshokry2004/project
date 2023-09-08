6db2ddb7e1666553315ff001f74328281a8ed237
mahmoudshokry2004 <113770820+mahmoudshokry2004@users.noreply.github.com>
2023-09-09 01:13:18 +0300

    Add files via upload

---
+++ reverse numbers.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int values[5];
+
+int input;
+
+cout<<"Please, Enter array of 5 numbers :\n";
+
+for(int i =0 ; i<5 ; i++)
+{
+
+cin>>input;
+ values[i]=input;
+
+}
+cout<<"{";
+for(int j=4 ; j>=0 ; j--)
+{
+
+cout<<values[j];
+    
+if(j>0)
+    
+cout<<" , ";
+
+}
+
+cout<<"}";
+   
+
+ 
+return 0;
+
+
+}
