48ed9289fa0d25dd5793ec594319d2de688cb6e0
mahmoudshokry2004 <113770820+mahmoudshokry2004@users.noreply.github.com>
2023-09-09 01:15:56 +0300

    Update reverse numbers.cpp

--- reverse numbers.cpp
+++ reverse numbers.cpp
@@ -1,39 +1,24 @@
 #include <iostream>
-
 using namespace std;
-
 int main() {
-
+ 
 int values[5];
-
 int input;
-
 cout<<"Please, Enter array of 5 numbers :\n";
 
 for(int i =0 ; i<5 ; i++)
 {
-
 cin>>input;
  values[i]=input;
-
 }
 cout<<"{";
 for(int j=4 ; j>=0 ; j--)
 {
-
-cout<<values[j];
-    
-if(j>0)
-    
+cout<<values[j];   
+if(j>0)  
 cout<<" , ";
-
 }
-
 cout<<"}";
-   
-
  
 return 0;
-
-
 }
